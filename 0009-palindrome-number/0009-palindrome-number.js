94eb15d4b62e523a6be41ca5c6c9dcf5d10df42f
Anit <110488522+kand5950@users.noreply.github.com>
2023-01-11 01:58:01 -0500

    Time: 168 ms (91.28%), Space: 51.7 MB (15.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let reverseInput = [...(x.toString())].reverse().join('')
+    
+    return reverseInput === x.toString();
+    
+};
